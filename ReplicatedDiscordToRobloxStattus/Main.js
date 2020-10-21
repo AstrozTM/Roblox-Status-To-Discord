243c05134d4c795f0d19dcbc2b2f54dd4acf3ebf
Astroz™ <61568620+AstrozTM@users.noreply.github.com>
2020-10-21 12:54:41 -0400

    Add files via upload

---
+++ ReplicatedDiscordToRobloxStattus/Main.js
@@ -0,0 +1,47 @@
+const Urls = require('./JSON/Urls.json');
+const Fetch = require('node-fetch')
+const Settings = require('./JSON/Settings.json')
+const colors = require('colors');
+let Status = ''
+function UpdateToDiscord() {
+    if(Status !== '') {
+       Fetch(Urls.DiscordApiUrl,{
+           method: "PATCH",
+           headers: {'authorization': Settings.Token,'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.308 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36',
+           'content-type': 'application/json',},
+           body: JSON.stringify({
+            custom_status: {
+            expires_at: null,
+            text: Status + '᲼',
+            emoji_id: "682740907833163822",
+            emoji_name: "Blushy"
+            }
+           })
+
+       }).then(ResponseApi => {
+        if (ResponseApi.status !== 200) {
+            console.log('Failed at setting discord\'s stattus. Code: ' + toString(ResponseApi.status).red)
+        }})
+    }
+    }
+
+function UpdateStattus() {
+    Fetch(Urls.robloxApiUrl + Settings.UserId + '/status', {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+    }).then(ResponseApi => {
+        if (ResponseApi.status !== 200) {
+            console.log('Failed at getting roblox\'s stattus. Code: ' + toString(ResponseApi.status).red)
+        }
+        return ResponseApi.text()
+    }).then(Txt => {
+        let Res = JSON.parse(Txt)
+        if (Status !== Res.status){
+        Status = Res.status
+        UpdateToDiscord()
+        console.log(`Updated Discord Status to: ${Status}`.blue)
+        }
+    })
+}
+
+setInterval(function(){UpdateStattus()},3000)
